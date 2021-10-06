036935fdb7c77a1990f88ff5c9b5698994d49b0b
HelenHorner <volkoff-tie@yandex.ru>
2021-10-06 16:05:47 +0300

    improve lint problems

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -12,27 +12,20 @@ const makeLine = (val, depth) => {
 const stylish = (diff, depth = 1, space = 4) => {
   const iter = (item) => {
     const replacer = ' ';
-    let result = '';
     switch (item.type) {
       case 'nested':
-        result = [`${replacer.repeat(depth * space)}${item.key}: ${stylish(item.children, depth + 1)}`];
-        break;
+        return [`${replacer.repeat(depth * space)}${item.key}: ${stylish(item.children, depth + 1)}`];
       case 'added':
-        result = [`${replacer.repeat(depth * space - 2)}+ ${item.key}: ${makeLine(item.value, depth)}`];
-        break;
+        return [`${replacer.repeat(depth * space - 2)}+ ${item.key}: ${makeLine(item.value, depth)}`];
       case 'deleted':
-        result = [`${replacer.repeat(depth * space - 2)}- ${item.key}: ${makeLine(item.value, depth)}`];
-        break;
+        return [`${replacer.repeat(depth * space - 2)}- ${item.key}: ${makeLine(item.value, depth)}`];
       case 'changed':
-        result = [[`${replacer.repeat(depth * space - 2)}- ${item.key}: ${makeLine(item.value, depth)}`], [`${replacer.repeat(depth * space - 2)}+ ${item.key}: ${makeLine(item.newValue, depth)}`]].join('\n');
-        break;
+        return [[`${replacer.repeat(depth * space - 2)}- ${item.key}: ${makeLine(item.value, depth)}`], [`${replacer.repeat(depth * space - 2)}+ ${item.key}: ${makeLine(item.newValue, depth)}`]].join('\n');
       case 'unchanged':
-        result = [`${replacer.repeat(depth * space - 2)}  ${item.key}: ${makeLine(item.value, depth)}`];
-        break;
+        return [`${replacer.repeat(depth * space - 2)}  ${item.key}: ${makeLine(item.value, depth)}`];
       default:
         throw new Error('Error AST');
     }
-    return result;
   };
   const { children } = diff;
   const lines = children.map((line) => iter(line, depth));
